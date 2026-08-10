40f01c6e58ecbb48dbb2302ff4f06e542418f13f
ujjawal <ujjawalrai0205@gmail.com>
2026-08-10 22:02:34 +0530

    Function20done\

--- Functions.js
+++ Functions.js
@@ -22,3 +22,42 @@ function ShoppingCart1(num1,num2,...num3) {
 console.log(ShoppingCart1(5867867,10,20,30,40,50));
 
 // 5867867 goes to num1, 10 goes to num2, and the rest of the values (20, 30, 40, 50) are captured in the num3 array. The function returns the num3 array containing [20, 30, 40, 50].
+
+const user = {
+    username: "JohnDoe",
+    email: "johndoe9789788@gmail.com",
+}
+
+
+function handleObject() // here we pass anyObject as a parameter to the function because we may create any function here named user , anyobject give us a local variable to work with so inside function use anyobject.username whatever outside while calling just pass orginal object
+{
+    console.log(`Username is ${user.username} and email is ${user.email}`);
+}
+
+function handleObject1(anyObject)  //overall anyObject is just generic name for the parameter that can be used to refer to any object passed to the function. It allows the function to work with different objects without being tied to a specific one.
+
+{ 
+    console.log(`Username is ${anyObject.username} and email is ${anyObject.email}`);
+}
+
+handleObject(user);
+handleObject1({
+    username: "JaneDorgtreegtre",
+    email: "janedoe9789788@gmail.comfghjsdik"
+});
+
+
+
+
+
+// Passing Array as an Argument to a Function
+
+const array = [1,2,3,4,5,6,7,8,9];
+
+function returnArray(myarr){
+    console.log(myarr);
+}
+
+returnArray(array);
+
+// same it can be passed directly without creating a variable like above in object example
